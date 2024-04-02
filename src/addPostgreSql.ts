f2a9a961726ecf07c7fb059aacdac7d8c1a738a7
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2024-04-02 18:43:51 +0200

    si

--- src/addPostgreSql.ts
+++ src/addPostgreSql.ts
@@ -1,38 +1,38 @@
-import express from "express";
-import "express-async-errors";
-import morgan from "morgan";
-import dotenv from "dotenv";
-dotenv.config();
+// import express from "express";
+// import "express-async-errors";
+// import morgan from "morgan";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-import {
-  getAll,
-  getOneById,
-  create,
-  updateById,
-  deleteById,
-} from "./controllers/planets.js";
+// import {
+//   getAll,
+//   getOneById,
+//   create,
+//   updateById,
+//   deleteById,
+// } from "./controllers/planets.js";
 
-const port = process.env.PORT;
-const app = express();
+// const port = process.env.PORT;
+// const app = express();
 
-app.use(express.json());
-app.use(morgan("dev"));
+// app.use(express.json());
+// app.use(morgan("dev"));
 
-// ottenere tutti i pianeti
-app.get("/api/planets", getAll);
+// // ottenere tutti i pianeti
+// app.get("/api/planets", getAll);
 
-// ottenere un pianeta specifico
-app.get("/api/planets/:id", getOneById);
+// // ottenere un pianeta specifico
+// app.get("/api/planets/:id", getOneById);
 
-// aggiunge un nuovo pianeta
-app.post("/api/planets", create);
+// // aggiunge un nuovo pianeta
+// app.post("/api/planets", create);
 
-// modifica un pianeta
-app.put("/api/planets/:id", updateById);
+// // modifica un pianeta
+// app.put("/api/planets/:id", updateById);
 
-// elimina un pianeta
-app.delete("/api/planets/:id", deleteById);
+// // elimina un pianeta
+// app.delete("/api/planets/:id", deleteById);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
