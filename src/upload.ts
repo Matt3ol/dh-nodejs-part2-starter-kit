503ab0a21b5b8c8ef177c1f49984b313f8eb62a4
Matt3ol <148490319+Matt3ol@users.noreply.github.com>
2024-04-02 19:14:26 +0200

    si

--- src/upload.ts
+++ src/upload.ts
@@ -1,58 +1,58 @@
-import express from "express";
-import "express-async-errors";
-import morgan from "morgan";
-import dotenv from "dotenv";
-dotenv.config();
+// import express from "express";
+// import "express-async-errors";
+// import morgan from "morgan";
+// import dotenv from "dotenv";
+// dotenv.config();
 
-import {
-  getAll,
-  getOneById,
-  create,
-  updateById,
-  deleteById,
-  createImage
-} from "./controllers/planet.js";
+// import {
+//   getAll,
+//   getOneById,
+//   create,
+//   updateById,
+//   deleteById,
+//   createImage
+// } from "./controllers/planet.js";
 
-import multer from "multer";
-const storage = multer.diskStorage({
-  destination: (req:any, file:any, cb:any) => {
-    cb(null, "./uploads")
-  },
-  filename: (req:any, file:any, cb:any) => {
-    cb(null, file.originalname)
-  }
+// import multer from "multer";
+// const storage = multer.diskStorage({
+//   destination: (req:any, file:any, cb:any) => {
+//     cb(null, "./uploads")
+//   },
+//   filename: (req:any, file:any, cb:any) => {
+//     cb(null, file.originalname)
+//   }
 
-})
-const upload = multer({ storage })
+// })
+// const upload = multer({ storage })
 
-const port = process.env.PORT;
-const app = express();
+// const port = process.env.PORT;
+// const app = express();
 
-app.use("/uploads", express.static("uploads"));
-app.use("/static", express.static("static"))
-
-
-app.use(express.json());
-app.use(morgan("dev"));
-
-// ottenere tutti i pianeti
-app.get("/api/planets", getAll);
-
-// ottenere un pianeta specifico
-app.get("/api/planets/:id", getOneById);
-
-// aggiunge un nuovo pianeta
-app.post("/api/planets", create);
-
-// modifica un pianeta
-app.put("/api/planets/:id", updateById);
-
-// elimina un pianeta
-app.delete("/api/planets/:id", deleteById);
-
-app.post("/api/planets/:id/image", upload.single("image"), createImage)
+// app.use("/uploads", express.static("uploads"));
+// app.use("/static", express.static("static"))
 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.use(express.json());
+// app.use(morgan("dev"));
+
+// // ottenere tutti i pianeti
+// app.get("/api/planets", getAll);
+
+// // ottenere un pianeta specifico
+// app.get("/api/planets/:id", getOneById);
+
+// // aggiunge un nuovo pianeta
+// app.post("/api/planets", create);
+
+// // modifica un pianeta
+// app.put("/api/planets/:id", updateById);
+
+// // elimina un pianeta
+// app.delete("/api/planets/:id", deleteById);
+
+// app.post("/api/planets/:id/image", upload.single("image"), createImage)
+
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
